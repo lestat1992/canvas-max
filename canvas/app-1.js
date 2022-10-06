d9c58cb4bb7070d8064344bd2e45accd93432c8c
MassimilianoBaldacci <massimiliano.baldacci@drivegsy.online>
2022-10-06 14:12:17 +0200

    FUNZIONA!

--- canvas/app-1.js
+++ canvas/app-1.js
@@ -2,31 +2,85 @@ import CanvasSize from "./class/CanvasSize";
 import GradientFullPage from "./class/GradientFullPage";
 import Node from "./class/Node";
 
+import getEase from "./functions/animation/getEase";
+
 //ELEMENTO CANVAS
 const canvas = document.querySelector("#hero-canvas");
 
-//responsive units
-let CSizes = new CanvasSize(canvas);
-function vw(percentage) {
-  return CSizes.vw(percentage);
-}
-function vh(percentage) {
-  return CSizes.vh(percentage);
-}
-let client = CSizes.client;
+let isAnimation = true;
 
-let isAnimation = false;
+function getX(params) {
+  let distance = params.xTo - params.xFrom;
+  let steps = params.frames;
+  let currentProgress = params.frame;
+  return getEase(currentProgress, params.xFrom, distance, steps, params.type);
+}
+
+function getY(params) {
+  let distance = params.yTo - params.yFrom;
+  let steps = params.frames;
+  let currentProgress = params.frame;
+  return getEase(currentProgress, params.yFrom, distance, steps, params.type);
+}
+
+//########################
+
+let endPosition;
 
 //RENDER ##################
+let frame = 0;
+
 window.requestAnimationFrame(render);
 
 function render() {
   //INITIAL SETTINGS
-  const time = new Date();
-  //Creating context
+  frame++;
+
+  //responsive units #################
+  let CSizes = new CanvasSize(canvas);
+  function vw(percentage) {
+    return CSizes.vw(percentage);
+  }
+  function vh(percentage) {
+    return CSizes.vh(percentage);
+  }
+  let client = CSizes.client;
+
+  //###################################
+  //ANIMATABLE ########################
+  let startPosition = { x: 0, y: vh(80) };
+
+  endPosition = {
+    //x: vw(30),
+    x: getX({
+      xTo: vw(50),
+      xFrom: vw(40),
+      frame: frame,
+      frames: 400,
+      type: "exponential",
+    }),
+    //vh(80) - vw(15),
+    y: getY({
+      yTo: vh(80) - vw(30),
+      yFrom: vh(40) - vw(0),
+      frame: frame,
+      frames: 400,
+      type: "exponential",
+    }),
+  };
+
+  console.log(frame + " == " + 400);
+  if (frame >= 400) {
+    isAnimation = false;
+  }
+
+  //###################################
+
+  //Creating context###################
   const ctx = canvas.getContext("2d");
   ctx.canvas.width = client.width;
   ctx.canvas.height = client.height;
+  //###################################
 
   let newBackground = new GradientFullPage(ctx, {
     type: "linear",
@@ -42,6 +96,7 @@ function render() {
       { position: 0.0, color: "lightblue" },
       { position: 1, color: "red" },
     ],
+
     /*
     length: 100,
     angle: 0,
@@ -53,8 +108,8 @@ function render() {
   let Node1 = new Node(ctx, {
     stroke: vw(0.5),
     pointSize: vw(1),
-    startPosition: { x: 0, y: vh(80) },
-    endPosition: { x: vw(30), y: vh(80) - vw(15) },
+    startPosition: startPosition,
+    endPosition: endPosition,
     headWidth: vw(15),
     headType: 1,
     borderRadious: vw(0.5),
---
+++ canvas/functions/animation/getEase.js
@@ -0,0 +1,85 @@
+function getEase(currentProgress, start, distance, steps, animationFunction) {
+  function getQuadraticEase(currentProgress, start, distance, steps) {
+    currentProgress /= steps / 2;
+    if (currentProgress <= 1) {
+      return (distance / 2) * currentProgress * currentProgress + start;
+    }
+    currentProgress--;
+
+    return (
+      1 * (distance / 2) * (currentProgress * (currentProgress - 2) - 1) + start
+    );
+  }
+
+  function getCubicEase(currentProgress, start, distance, steps) {
+    currentProgress /= steps / 2;
+    if (currentProgress < 1) {
+      return (distance / 2) * Math.pow(currentProgress, 3) + start;
+    }
+    currentProgress -= 2;
+    return (distance / 2) * (Math.pow(currentProgress, 3) + 2) + start;
+  }
+
+  function getQuarticEase(currentProgress, start, distance, steps) {
+    currentProgress /= steps / 2;
+    if (currentProgress < 1) {
+      return (distance / 2) * Math.pow(currentProgress, 4) + start;
+    }
+    currentProgress -= 2;
+    return ((-1 * distance) / 2) * (Math.pow(currentProgress, 4) - 2) + start;
+  }
+
+  function getQuinticEase(currentProgress, start, distance, steps) {
+    currentProgress /= steps / 2;
+    if (currentProgress < 1) {
+      return (distance / 2) * Math.pow(currentProgress, 5) + start;
+    }
+    currentProgress -= 2;
+    return (distance / 2) * (Math.pow(currentProgress, 5) + 2) + start;
+  }
+
+  function getSinusoidalEase(currentProgress, start, distance, steps) {
+    return (
+      (-distance / 2) * (Math.cos((Math.PI * currentProgress) / steps) - 1) +
+      start
+    );
+  }
+
+  function getExponentialEase(currentProgress, start, distance, steps) {
+    currentProgress /= steps / 2;
+
+    if (currentProgress < 1) {
+      return (distance / 2) * Math.pow(2, 10 * (currentProgress - 1)) + start;
+    }
+    currentProgress--;
+
+    return (distance / 2) * (-Math.pow(2, -10 * currentProgress) + 2) + start;
+  }
+
+  switch (animationFunction) {
+    case "static":
+      return cardPadding;
+      break;
+
+    case "quadratic": //??Non funziona
+      return getQuadraticEase(currentProgress, start, distance, steps);
+      break;
+    case "cubic":
+      return getCubicEase(currentProgress, start, distance, steps);
+      break;
+    case "quartic":
+      return getQuarticEase(currentProgress, start, distance, steps);
+      break;
+    case "quintic":
+      return getQuinticEase(currentProgress, start, distance, steps);
+      break;
+    case "sinusoidal":
+      return getSinusoidalEase(currentProgress, start, distance, steps);
+      break;
+    case "exponential":
+      return getExponentialEase(currentProgress, start, distance, steps);
+      break;
+  }
+}
+
+export default getEase;
