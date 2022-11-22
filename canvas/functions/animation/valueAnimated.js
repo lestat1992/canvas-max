a95888cc0574fac7be1df0a844b610d7d84a1725
MassimilianoBaldacci <massimiliano.baldacci@drivegsy.online>
2022-11-22 17:18:41 +0100

    X è stato corretto

--- canvas/functions/animation/valueAnimated.js
+++ canvas/functions/animation/valueAnimated.js
@@ -11,19 +11,19 @@ import getEase from "./getEase";
 */
 
 function getX(params) {
-    const { xTo, xFrom, frame, frames } = params;
-    //console.log(frame, frames);
-    let percentagePocede = (frame / frames) * 100;
-    let distance = xTo - xFrom;
-    let result = xTo + (distance / percentagePocede) * percentagePocede;
-    //console.log(frame, frames, percentagePocede);
-    if (params.i) console.log(result);
+  const { xTo, xFrom, frame, frames } = params;
+  //console.log(frame, frames);
+  let percentagePocede = (frame / frames) * 100;
+  let distance = xTo - xFrom;
+  let result = xFrom + (percentagePocede * distance) / 100; //(distance / percentagePocede) * percentagePocede;
+  //console.log(frame, frames, percentagePocede);
+  if (params.i) console.log(result);
 
-    return result;
+  return result;
 
-    //FRAME é SBAGLIATO A OGNI NUOVO KEYFRAME DEVE PERTIRE DA 0
+  //FRAME é SBAGLIATO A OGNI NUOVO KEYFRAME DEVE PERTIRE DA 0
 
-    /*
+  /*
   let distance = params.xTo - params.xFrom;
   let steps = params.frames;
   let currentProgress = params.frame;
@@ -32,13 +32,13 @@ function getX(params) {
 }
 
 function getY(params) {
-    const { yTo, yFrom, frame, frames } = params;
-    let percentagePocede = (frames / frame) * 100;
-    let distance = yTo - yFrom;
-    let result = yTo + (distance / percentagePocede) * percentagePocede;
-    return result;
+  const { yTo, yFrom, frame, frames } = params;
+  let percentagePocede = (frames / frame) * 100;
+  let distance = yTo - yFrom;
+  let result = yTo + (distance / percentagePocede) * percentagePocede;
+  return result;
 
-    /*
+  /*
   
   let distance = params.yTo - params.yFrom;
   let steps = params.frames;
